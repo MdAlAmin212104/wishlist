a2849155b04ff00bda347471d1ce16e4e02bfbd2
MdAlAmin212104 <mdalamin212104@gmail.com>
2025-07-19 08:39:16 +0600

    handle data from input filed

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -1,83 +1,96 @@
+import { Form, useLoaderData, useActionData } from "@remix-run/react";
 import {
   Box,
   Card,
-  Layout,
-  Link,
-  List,
   Page,
   Text,
   BlockStack,
+  InlineGrid,
+  TextField,
+  Button,
 } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
+import { useState, useEffect } from "react";
+import { json } from "@remix-run/node";
 
-export default function SettingsPage() {
-  return (
-    <Page>
-      <TitleBar title="settings" />
-      <Layout>
-        <Layout.Section>
-          <Card>
-            <BlockStack gap="300">
-              <Text as="p" variant="bodyMd">
-                The app template comes with an additional page which
-                demonstrates how to create multiple pages within app navigation
-                using{" "}
-                <Link
-                  url="https://shopify.dev/docs/apps/tools/app-bridge"
-                  target="_blank"
-                  removeUnderline
-                >
-                  App Bridge
-                </Link>
-                .
-              </Text>
-              <Text as="p" variant="bodyMd">
-                To create your own page and have it show up in the app
-                navigation, add a page inside <Code>app/routes</Code>, and a
-                link to it in the <Code>&lt;NavMenu&gt;</Code> component found
-                in <Code>app/routes/app.jsx</Code>.
-              </Text>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section variant="oneThird">
-          <Card>
-            <BlockStack gap="200">
-              <Text as="h2" variant="headingMd">
-                Resources
-              </Text>
-              <List>
-                <List.Item>
-                  <Link
-                    url="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-                    target="_blank"
-                    removeUnderline
-                  >
-                    App nav best practices
-                  </Link>
-                </List.Item>
-              </List>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
+export async function loader() {
+  let settings = {
+    name: "My app",
+    description: "My app description",
+  };
+  return json(settings);
 }
 
-function Code({ children }) {
+export async function action({ request }) {
+  const formData = await request.formData();
+  const settings = Object.fromEntries(formData);
+  // Save to DB logic here...
+  return json(settings);
+}
+
+export default function SettingsPage() {
+  const loaderData = useLoaderData();
+  const actionData = useActionData();
+  const [formState, setFormState] = useState(loaderData);
+
+  useEffect(() => {
+    if (actionData) {
+      setFormState(actionData);
+    }
+  }, [actionData]);
+
   return (
-    <Box
-      as="span"
-      padding="025"
-      paddingInlineStart="100"
-      paddingInlineEnd="100"
-      background="bg-surface-active"
-      borderWidth="025"
-      borderColor="border"
-      borderRadius="100"
+    <Page
+      divider
+      primaryAction={{ content: "View on your store", disabled: true }}
+      secondaryActions={[
+        {
+          content: "Duplicate",
+          accessibilityLabel: "Secondary action label",
+          onAction: () => alert("Duplicate action"),
+        },
+      ]}
     >
-      <code>{children}</code>
-    </Box>
+      <BlockStack gap={{ xs: "800", sm: "400" }}>
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Settings
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Update app Settings and preferences.
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <Form method="POST">
+              <BlockStack gap="400">
+                <TextField
+                  label="Interjamb style"
+                  name="name"
+                  value={formState.name}
+                  onChange={(value) =>
+                    setFormState({ ...formState, name: value })
+                  }
+                />
+                <TextField
+                  label="Interjamb ratio"
+                  name="description"
+                  value={formState.description}
+                  onChange={(value) =>
+                    setFormState({ ...formState, description: value })
+                  }
+                />
+                <Button submit>Save</Button>
+              </BlockStack>
+            </Form>
+          </Card>
+        </InlineGrid>
+      </BlockStack>
+    </Page>
   );
 }
