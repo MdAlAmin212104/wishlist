7b734af9abb0824b2d1395eb92e43bcb8b443c77
MdAlAmin212104 <mdalamin212104@gmail.com>
2025-07-21 00:58:35 +0600

    create on wishlist item and save

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,30 +1,45 @@
 import { json } from "@remix-run/node";
+import { cors } from "remix-utils/cors";
 
-export async function loader() {
-  let settings = {
-    name: "My app",
-    description: "My app description",
-    
-  };
-  return json(settings);
-}
+import db from "../db.server"
+
+export async function action({ request }) {
+  const method = request.method;
+  let data = await request.formData();
+  data = Object.fromEntries(data);
+  const customerId = data.customerId;
+  const productId = data.productId;
+  const shop = data.shop;
 
 
-export async function action({ request}){
-    const method = request.method;
+  if (!customerId || !productId || !shop) {
+    return json({
+      message: "Missing data. Required: customerId, productId, shop",
+      method,
+    });
+  }
 
-    switch(method){
-        case "POST":
-            // Handle POST reqeuest locig here
-            // For example, adding a new item to the wishlist
-            return json({message : "Success", method: "POST"});
-        case "PATCH":
-            // Handle POST reqeuest locig here
-            // For example, adding a new item to the wishlist\
-            return json ({message: "Success", method: "PATCH"})
-        default:
-            // optional: handle other methods or return a method not allowed response
-            return new Response("Method Not Allowed", {status: 405});
-    }
-    
+  switch (method) {
+    case "POST":
+      const wishlist = await db.wishlist.create({
+        data: {
+          customerId,
+          productId,
+          shop,
+        },
+      });
+
+      const response = json({
+        message: "Product added to wishlist",
+        method: "POST",
+        wishlist,
+      });
+
+      // eslint-disable-next-line no-undef
+      return cors(request, response); // assuming cors is defined
+    case "PATCH":
+      return json({ message: "Success", method: "PATCH" });
+    default:
+      return new Response("Method Not Allowed", { status: 405 });
+  }
 }
---
+++ prisma/migrations/20250720185159_init/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Settings` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Settings";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "settings" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Wishlist" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "customerId" TEXT,
+    "productId" TEXT,
+    "shop" TEXT,
+    "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
